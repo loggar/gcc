5c2da3fcfb52e6c3b762eacaae525c05fc376863
charly.lee <webnl@DT-Charly.koi.local>
2018-01-01 17:11:08 +1100

    primative data types

---
+++ practice-c/primative_data_types/totaller.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+
+float total = 0.0;
+short count = 0;
+short tax_percent = 6;
+
+float add_with_tax(float f)
+{
+	float tax_rate = 1 + tax_percent / 100.0;
+	total = total + (f * tax_rate);
+	count = count + 1;
+	return total;
+}
+
+int main()
+{
+	float val;
+	float arr[5] = {10.0, 20.0, 30.0, 40.0, 50.0};
+
+	for (int i = 0; i < 5; i++)
+	{
+		val = arr[i];
+		printf("Total so far: %.2f\n", add_with_tax(val));
+	}
+
+	printf("\nFinal total: %.2f\n", total);
+	printf("Number of items: %hi\n", count);
+	
+	return 0;
+}
